a2ddfbd43ab5ef9b47f81d6ff3c6ff4b0f2acffd
TarasZabochen <zabochen@gmail.com>
2020-08-11 15:00:43 +0300

    Init project

---
+++ app/src/main/java/ua/ck/zabochen/android/firebasecloudmessaging/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package ua.ck.zabochen.android.firebasecloudmessaging.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.ck.zabochen.android.firebasecloudmessaging.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
